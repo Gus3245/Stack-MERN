3c048793531bc2c745f79625eb433d18c2e55e65
Gus3245 <Enderdragon3245@gmail.com>
2025-01-11 16:54:24 -0300

    Feat: Create and Implementing tokens validation

--- Source/Controllers/news.Controller.js
+++ Source/Controllers/news.Controller.js
@@ -3,10 +3,30 @@ import NewsService from "../Services/news.Service.js";
 
 const create = async (req, res) => {
     try {
+        const {authorization} = req.headers;
+        console.log(authorization);
+
+        if (!authorization){
+            return res.send(401).send({message: "Not Authored"});
+        }
+
+        const parts = authorization.split(" ");
+        const [schema, token] = parts;
+
+        if(parts.length !==2){
+            return res.send(401).send({message: "Not Authored"});
+        }
+
+        if(schema !== "Bearer"){
+            return res.send(401).send({message: "Not Authored"});
+        }
+
+        
+
         const {title, content, banner} = req.body;
 
         if(!title || !content || !banner){
-            return res.status(400).send({message: "Bad Resquest"});
+            return res.status(400).send({message: "Submit All Fields"});
         }
 
         const CreatedNews = await NewsService.createService({
