fa9f469cd3d8a055acbf4150e2332a9d1cc79de6
Gus3245 <Enderdragon3245@gmail.com>
2025-01-11 16:25:27 -0300

    Feat: Create Controller for News

--- Source/Controllers/news.Controller.js
+++ Source/Controllers/news.Controller.js
@@ -1,10 +1,24 @@
 import NewsService from "../Services/news.Service.js";
 
-const create = (req, res) => {
+
+const create = async (req, res) => {
     try {
-        res.status(200).send({message: "News Created"});
+        const {title, content, banner} = req.body;
+
+        if(!title || !content || !banner){
+            return res.status(400).send({message: "Bad Resquest"});
+        }
+
+        const CreatedNews = await NewsService.createService({
+            title,
+            content,
+            banner,
+            user: {_id: "678025ab95f1e155fc59f6cb"}
+        });
+
+        res.status(200).send({CreatedNews});
     } catch (error) {
-        
+        res.status(500).send({message: error.message});
     }
 }
 
